318d666a06de264e2541541936d1f23ec0b56228
blank <69109590+iprashikmanwar@users.noreply.github.com>
2023-10-30 02:50:36 +0530

    008:IATCW include cross-origin annotation and configurationproperties

--- src/main/java/com/prashikmanwar/weatherapp/controller/contentnegotiation/v1/CurrentWeatherController.java
+++ src/main/java/com/prashikmanwar/weatherapp/controller/contentnegotiation/v1/CurrentWeatherController.java
@@ -1,5 +1,6 @@
 package com.prashikmanwar.weatherapp.controller.contentnegotiation.v1;
 
+import com.prashikmanwar.weatherapp.configuration.WeatherAppProperties;
 import com.prashikmanwar.weatherapp.model.CurrentWeatherResponse;
 import com.prashikmanwar.weatherapp.repository.CurrentWeatherCollectionRepository;
 import org.slf4j.Logger;
@@ -11,23 +12,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/v1/currentWeather")
 public class CurrentWeatherController {
     private final Logger logger = LoggerFactory.getLogger(CurrentWeatherController.class);
     private final CurrentWeatherCollectionRepository repository;
 
-    CurrentWeatherController(CurrentWeatherCollectionRepository repository) {
+    /*@Value("${weatherStack.basePath: www.google.com}")
+    //www.google.com - is a default value provided for the parameter weatherStack.basePath i.e. if not provided google.com will be picked
+    private String WEATHERSTACK_URL;*/
+    private final WeatherAppProperties weatherAppProperties;
+
+    CurrentWeatherController(CurrentWeatherCollectionRepository repository, WeatherAppProperties weatherAppProperties) {
         this.repository = repository;
+        this.weatherAppProperties = weatherAppProperties;
     }
 
     @GetMapping(value = {"/{location}"}, produces = {"application/json"})
     public ResponseEntity<CurrentWeatherResponse> getCurrentWeatherByLocation(@PathVariable String location) {
         try {
             logger.info("Request received for getting weather at {}", location);
-            CurrentWeatherResponse currentWeatherResponse = new RestTemplate().getForEntity("http://api.weatherstack.com/current?access_key=27c50212cc3694b06738080e84844a3f&query=" + location, CurrentWeatherResponse.class).getBody();
+            CurrentWeatherResponse currentWeatherResponse = new RestTemplate().getForEntity(weatherAppProperties.basePath() + location, CurrentWeatherResponse.class).getBody();
             logger.debug("Response received from external service for {} - {}", currentWeatherResponse.location().name(), currentWeatherResponse.current().weather_descriptions());
             repository.save(currentWeatherResponse);
             return ResponseEntity.ok(currentWeatherResponse);
